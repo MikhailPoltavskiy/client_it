ecd5bc1cb52dad36add38983edd2e61ade34a89b
MikhailPoltavskiy <100789978+MikhailPoltavskiy@users.noreply.github.com>
2023-08-28 10:38:21 +0300

    lesson 5.15

--- lib/feature/auth/ui/login_screen.dart
+++ lib/feature/auth/ui/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:client_it/app/ui/components/app_text_button.dart';
 import 'package:client_it/app/ui/components/app_text_field.dart';
+import 'package:client_it/feature/auth/domain/auth_state/auth_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
@@ -40,7 +42,7 @@ class LoginScreen extends StatelessWidget {
                 AppTextButton(
                   onPressed: () {
                     if (formKey.currentState?.validate() == true) {
-                      print('OK');
+                      _onTapToSignIn(context.read<AuthCubit>());
                     }
                   },
                   textButton: 'sign in',
@@ -60,4 +62,9 @@ class LoginScreen extends StatelessWidget {
       ),
     );
   }
+
+  void _onTapToSignIn(AuthCubit authCubit) => authCubit.signIn(
+        username: controllerLogin.text,
+        password: controllerPassword.text,
+      );
 }
