36c613313d3795d69de7b4c03590ab4c8739e447
MikhailPoltavskiy <100789978+MikhailPoltavskiy@users.noreply.github.com>
2023-08-31 09:16:11 +0300

    lesson 5.18

--- lib/feature/auth/data/dto/user_dto.dart
+++ lib/feature/auth/data/dto/user_dto.dart
@@ -1,3 +1,4 @@
+import 'package:client_it/feature/auth/domain/entities/user_entity/user_entity.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'user_dto.g.dart';
@@ -21,4 +22,14 @@ class UserDto {
   factory UserDto.fromJson(Map<String, dynamic> json) =>
       _$UserDtoFromJson(json);
   Map<String, dynamic> toJson() => _$UserDtoToJson(this);
+
+  UserEntity toEntity() {
+    return UserEntity(
+      email: email.toString(),
+      username: username.toString(),
+      id: id.toString(),
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    );
+  }
 }
