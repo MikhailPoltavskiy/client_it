33cf464f0464ae42c7655d1e7235c86ae545079e
MikhailPoltavskiy <100789978+MikhailPoltavskiy@users.noreply.github.com>
2023-09-07 09:20:13 +0300

    lesson_5.20

--- lib/app/ui/main_app_runner.dart
+++ lib/app/ui/main_app_runner.dart
@@ -2,6 +2,8 @@ import 'package:client_it/app/di/init_di.dart';
 import 'package:client_it/app/domain/app_builder.dart';
 import 'package:client_it/app/domain/app_runner.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
 class MainAppRunner implements AppRunner {
   final String env;
@@ -20,6 +22,11 @@ class MainAppRunner implements AppRunner {
   @override
   Future<void> run(AppBuilder appBuilder) async {
     await preloadData();
-    runApp(appBuilder.buildApp());
+    final storage = await HydratedStorage.build(
+        storageDirectory: await getApplicationDocumentsDirectory());
+    HydratedBlocOverrides.runZoned(
+      () => runApp(appBuilder.buildApp()),
+      storage: storage,
+    );
   }
 }
